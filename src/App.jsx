f7e42fc4b774a1bc6079cc8deb7ceb702d100e82
Mrinank Gaur <mrinank2484@gmail.com>
2025-02-12 21:03:40 +0530

    inital commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Navigation from './components/Navigation/Navigation'
+import ContactHeader from './components/ContactHeaer/ContactHeader'
+
+function App() {
+
+  return (
+    <div>
+      <Navigation/>
+      <ContactHeader/>
+    </div>
+   
+  )
+}
+
+
+export default App
---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,14 @@
+import styles from "./ContactForm.module.css";
+
+const ContactForm = () => {
+  return (
+    <section className={styles.container}>
+        <div className={styles.contact_form}></div>
+        <div className={styles.contactImages}> 
+
+        </div>
+    </section>
+  )
+}
+
+export default ContactForm;
